3a384a38b214970c6b5d2dac61176d6e833fbb00
Karim KOLLI <karim.kolli@gmail.com>
2019-06-09 17:58:35 +0200

    upgrade to Material 8

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import {
-  MatButtonModule,
-  MatTableModule,
-  MatChipsModule,
-  MatProgressSpinnerModule,
-  MatCardModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
 
 const Material = [
   MatButtonModule,
